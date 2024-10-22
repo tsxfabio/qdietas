635a69e32b43906b378d11a698664991d99ceb02
Fabio Oliva <contato@fabiooliva.dev>
2024-10-21 23:40:35 -0300

    chore: add users route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,51 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
-export const prisma = new PrismaClient()
+
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    birthDate: z.string().refine((date) => !isNaN(Date.parse(date)), {
+      message: 'Invalid date format',
+    }),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, birthDate, email, password } = registerBodySchema.parse(
+    request.body,
+  )
+
+  try {
+    const newUser = await prisma.$transaction(async (prisma) => {
+      // Criar o usuário na tabela `User`
+      const user = await prisma.user.create({
+        data: {
+          name,
+          birth_date: new Date(birthDate),
+        },
+      })
+
+      // Criar o auth vinculado ao usuário
+      const auth = await prisma.auth.create({
+        data: {
+          email,
+          password_hash: password,
+          user: {
+            connect: {
+              id: user.id,
+            },
+          },
+        },
+      })
+
+      return { user, auth }
+    })
+
+    reply.send(newUser)
+  } catch (error) {
+    reply.status(500).send({ error: 'Erro ao criar o usuário e o auth' })
+  }
+})
